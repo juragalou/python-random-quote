a5e07a9f1f82a3b97e0bc2f97242fda28daf3e03
juragalou <73969309+juragalou@users.noreply.github.com>
2020-11-05 12:18:05 +0100

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def primary():
   # print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,6 +8,5 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
-# python3 get-quote.py
-git commit -m "Hello World"
+  primary()
+
